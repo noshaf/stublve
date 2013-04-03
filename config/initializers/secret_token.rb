22beb090ede1c24dd4f6908acf9a329ecd701739
Noah Shafi <noshaf@gmail.com>
2013-04-03 13:27:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stublve::Application.config.secret_token = 'f458d65f0aa9498022a0a180cb663ad93a884ab8db47a2fedc64e6d08103c4e5eab338f71c85902cececca92849a43e04cea165e87619ccc6919dad0f3bc8805'
